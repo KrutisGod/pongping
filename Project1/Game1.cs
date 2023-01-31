98f2bc5ff51b1cab5291cc0f524f62c5d1a625e6
Krutis <krutis@spstrutnov.cz>
2023-01-31 09:20:30 +0100

    micek

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -9,6 +9,8 @@ namespace PongKrutis
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Palka palka;
+        private Micek micek;
         public int vyskaOkna = 800;
         public int sirkaOkna = 600;
 
@@ -25,13 +27,15 @@ namespace PongKrutis
             _graphics.PreferredBackBufferWidth = vyskaOkna;
             _graphics.PreferredBackBufferHeight = sirkaOkna;
             _graphics.ApplyChanges();
-
+                
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            palka = new Palka(100, 300, 12, 18, 125, GraphicsDevice, Keys.Up, Keys.Down, Color.Black);
+            micek = new Micek(700, 300, 6, 6,GraphicsDevice, Color.Black, 15);
 
             // TODO: use this.Content to load your game content here 
         }
@@ -42,13 +46,32 @@ namespace PongKrutis
                 Exit();
 
             // TODO: Add your update logic here 
+            palka.Aktualizovat(vyskaOkna, sirkaOkna);
+            micek.aktualizovat(vyskaOkna, sirkaOkna);
+
+            Rectangle micekKolize = new Rectangle((int)micek._pozice.X, (int)micek._pozice.Y, micek._prumer, micek._prumer);
+            Rectangle palkaKolize = new Rectangle((int)palka._pozice.X, (int)palka._pozice.Y, palka._velikostX, palka._velikostY);
             
+            if (micekKolize.Intersects(palkaKolize))
+            {
+                micek._rychlost = Vector2.Reflect(micek._rychlost, new Vector2(-1, 0));
+            }
+            if(micek._pozice.X <= 0 + micek._textura.Width)
+            {
+                micek._rychlost.X ++;
+                micek._rychlost.Y ++;
+                micek._pozice.X = 700;
+                micek._pozice.Y = 300;
+                if (micek._rychlost.X == 20)
+                {
+                    micek._rychlost.X = 6;
+                    micek._rychlost.Y = 6;
+                }
+                
+            }
 
-   
-       
-
-
-
+                
+           
             base.Update(gameTime);
         }
 
@@ -58,8 +81,10 @@ namespace PongKrutis
 
             // TODO: Add your drawing code here 
             _spriteBatch.Begin();
+            palka.Vykreslit(_spriteBatch);
+            micek.Vykreslit(_spriteBatch);
             _spriteBatch.End();
-
+           
 
             base.Draw(gameTime);
         }
---
+++ Project1/Micek.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+
+namespace PongKrutis
+{
+    class Micek
+    {
+        public Texture2D _textura;
+        public Vector2 _pozice;
+        
+
+        public int _prumer { get; private set; }
+
+        public Vector2 _rychlost;
+       
+        
+
+        
+
+        public Color _barva;
+
+        public Micek(float poziceX, float poziceY, float rychlostX, float rychlostY, GraphicsDevice zobrazovac, Color barva, int prumer)
+        {
+            _pozice.X = poziceX;
+            _pozice.Y = poziceY;
+            _rychlost.X = rychlostX;
+            _rychlost.Y = rychlostY;
+            
+            _prumer = prumer;
+            _barva = barva;
+
+            int polomer = prumer / 2;
+            Color[] pixely = new Color[prumer * prumer];
+            for (int i = 0; i < _prumer; i++)
+            {
+                for (int j = 0; j < _prumer; j++)
+                {
+                    if (Math.Sqrt(Math.Pow(j - polomer, 2)+ Math.Pow(i-polomer,2)) < polomer)
+                    {
+                        pixely[_prumer * i + j] = Color.Black;
+                    } else
+                    {
+                        pixely[_prumer * i + j] = Color.Transparent;
+                    }
+                }
+            }
+            _textura = new Texture2D(zobrazovac, _prumer, _prumer);
+            _textura.SetData(pixely);
+
+        }
+
+        public void aktualizovat (int sirkaOkna, int vyskaOkna)
+        {
+            
+            
+            _pozice.X += _rychlost.X;
+            _pozice.Y += _rychlost.Y;
+            
+
+            if(_pozice.Y >= vyskaOkna - _prumer || _pozice.Y <= 0 + _prumer)
+            {
+                _rychlost.Y *= -1;
+                
+            
+            }
+            if(_pozice.X >= sirkaOkna - _prumer || _pozice.X <= 0 + _prumer)
+            {
+                _rychlost.X *= -1;
+            }
+        }
+
+        public void Vykreslit(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_textura, _pozice, _barva);
+
+
+        }
+    }
+}
